e9189078d1377d782b76b1161cc11adc3ec06311
Istvan Uveges <uveges.istvan@gmail.com>
2024-06-09 17:14:08 +0200

    Added movies app

--- hellod_project/urls.py
+++ hellod_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("poll.urls")),
+    path('movies/', include('movies.urls')),
     path('admin/', admin.site.urls),
 ]
